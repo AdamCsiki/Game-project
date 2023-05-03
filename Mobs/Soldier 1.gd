c2b278224d592e1fd1bdbf774d44227d7bd81ef6
Kowalski7 <rudolf.daian01@e-uvt.ro>
2023-05-03 20:34:30 +0300

    Added mob spawner and path

---
+++ Mobs/Soldier 1.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+@export var speed = 1000
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	get_parent().set_progress(get_parent().get_progress() + delta * speed)
+	if get_parent().get_progress_ratio() == 1:
+		queue_free()
